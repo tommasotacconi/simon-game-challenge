9bb71c0c5d0d1b8d5b39113ad3b199302a7acb83
tommmasotacconi <tommaso.tacconi@gmail.com>
2025-01-22 06:55:10 +0100

    Added first logic to until game-over

--- js/game.js
+++ js/game.js
@@ -17,15 +17,22 @@ function playSound(name) {
  * @returns {number}
  */
 function nextSequence() {
+	// Remove the event listener at beginning game
+	if (!level) $('body').off('keypress');
+
 	const randomNumber = Math.floor(Math.random() * 4);
 	// Selects color with nextSequence function
 	let randomChosenColour = buttonColours[randomNumber];
 	gamePattern.push(randomChosenColour);
+	console.log(gamePattern);
 	// Select button with selected colour
 	const buttonSelected = $('div' + '#' + randomChosenColour + '.btn');
 	buttonSelected.animate({opacity: 0.2}).animate({opacity: 1});
-	// Reproduce audio
+	// Reproduces audio
 	playSound(randomChosenColour);
+	// Change title
+	$('h1').text('Level ' + level++);
+	console.log(level);
 }
 
 /**
@@ -41,21 +48,47 @@ function animatePress(currentColour) {
 	}, 100);
 }
 
+function checkAnswer(currentLevel) {
+	if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+		console.log('success', '---game pattern:', gamePattern[currentLevel], '---' );
+		return true
+	}
+	else {
+		console.log('wrong', '---game pattern:', gamePattern[currentLevel], '---');
+		return false
+	}
+}
+
 
 // SET UP PHASE
 const buttonColours = ['red', 'blue', 'green', 'yellow'];
 const gamePattern = [];
 const userClickedPattern = [];
-
+let level = 0;
 
 
 // PROCESSING PHASE
-nextSequence();
-
+// Checks whether the game has started or not before calling nextSequence
+// function when a keyboard keys get pressed
+if (!level) {
+	$('body').on('keypress', nextSequence);
+}
 // Handler function for when buttons are clicked
 $('div.btn').on('click', function (e) {
 	const userChosenColour = e.target.id;
 	playSound(userChosenColour);
 	animatePress(userChosenColour);
 	userClickedPattern.push(userChosenColour);
+	console.log(userClickedPattern);
+	// Checks if the added colour is correct remembering 
+	if (!checkAnswer(userClickedPattern.length - 1)) {
+		const newElement = $("<div id='result'></div>").text('Hai fatto un errore, riprova').css({color: '#fff', fontFamily: "'Press Start 2P', cursive"});
+		$('body').append(newElement);
+		$('.container').slideUp();
+		return
+	}
+	if (userClickedPattern.length === level) {
+		userClickedPattern.length = 0;
+		setTimeout(nextSequence, 2000);
+	}
 })
